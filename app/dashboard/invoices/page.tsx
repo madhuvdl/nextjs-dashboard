ea94413f6a3a9a35daeaeb4e3a0f7db803334b92
Madhubabu V <madhuvdl.mm@gmail.com>
2024-03-13 15:33:21 +0530

    initial files

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export const Page = () => {
+    return (
+        <p>Invoices Page</p>
+    )
+}
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export const Page = () => {
+    return (
+        <p>Dashboard Page</p>
+    )
+}
+
+export default Page;
